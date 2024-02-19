340daaadcc8240b65ec4147841543e02c097ca24
jawwaonline <jawwa@jawwa.de>
2024-02-19 13:31:12 +0100

    swipe working design fatal

---
+++  1-tinder/script.js
@@ -0,0 +1,65 @@
+// check for grabbing
+
+// check distance of swipe
+
+// remove element if it's too far
+
+// stop listening after grabbing
+
+const maxSwipeDistance = 170;
+let deltaX;
+let distanceX;
+let isAnimating = false; // <-still no idea
+
+function startgrab(event) {
+	if (isAnimating) {
+		return;
+	}
+	isAnimating = true;
+
+	const startGrabPoint = event.pageX;
+	const actualElement = event.target.closest('article');
+
+	if (!actualElement) {
+		return;
+	}
+	document.addEventListener('mousemove', moveDistance);
+	document.addEventListener('mouseup', stopGrab);
+
+	function moveDistance(e) {
+		distanceX = e.pageX - startGrabPoint;
+		actualElement.style.transform = `translateX(${distanceX}px) rotate(${
+			distanceX / 10
+		}deg)`;
+
+		if (distanceX < 0) {
+			actualElement.querySelector('#nope').style.opacity = '1';
+			actualElement.querySelector('#like').style.opacity = '0';
+		}
+		if (distanceX > 0) {
+			actualElement.querySelector('#like').style.opacity = '1';
+			actualElement.querySelector('#nope').style.opacity = '0';
+		}
+	}
+
+	function stopGrab(event) {
+		console.log('mouseup');
+		deltaX = Math.abs(distanceX);
+		if (deltaX > maxSwipeDistance) {
+			actualElement.style.transform = `translateX(${200}px) rotate(${
+				distanceX / 10
+			}deg)`;
+			actualElement.remove();
+		} else {
+			actualElement.style.transform = `translateX(0px) rotate(0deg)`;
+			actualElement.querySelector('#like').style.opacity = '0';
+			actualElement.querySelector('#nope').style.opacity = '0';
+		}
+		document.removeEventListener('mousemove', moveDistance);
+		document.removeEventListener('mouseup', stopGrab);
+
+		isAnimating = false;
+	}
+}
+
+document.addEventListener('mousedown', startgrab);
